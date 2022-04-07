dd109821c55b7e24b7de5e5f712359d83e627f96
Karol Zyśk <trener.karolzysk@gmail.com>
2022-04-07 23:25:08 +0200

    setting  up project / creating Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Nav,
+  Container,
+  Logo,
+  MobileIcon,
+  Menu,
+  Item,
+  NavLink,
+  NavBtn,
+  BtnLink,
+} from "./Navbar.styles";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Container>
+          <Logo to="/">Relax</Logo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <Menu>
+            <Item>
+              <NavLink to="about">About</NavLink>
+            </Item>
+            <Item>
+              <NavLink to="discover">Discover</NavLink>
+            </Item>
+            <Item>
+              <NavLink to="relax">Into Forest</NavLink>
+            </Item>
+            <Item>
+              <NavLink to="join">Join</NavLink>
+            </Item>
+          </Menu>
+          <NavBtn>
+            <BtnLink to="signin">Sign In</BtnLink>
+          </NavBtn>
+        </Container>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
